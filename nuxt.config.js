3464fc18869afc75f01fe56c58905fd1e7eee5f3
rokuzeudon <rokuzeudon@gmail.com>
2019-06-30 11:49:47 +0900

    change router for github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/bootstrap-xd-guide/favicon.ico' }
     ]
   },
   /*
@@ -45,5 +45,9 @@ export default {
     */
     extend(config, ctx) {
     }
+  },
+
+  router: {
+    base: '/bootstrap-xd-guide/'
   }
 }
