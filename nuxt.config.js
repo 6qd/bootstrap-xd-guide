0cb37b35f91eb3fd7c547e0cacbafd8fc18c1fbe
rokuzeudon <rokuzeudon@gmail.com>
2019-07-06 18:13:52 +0900

    Add GTM tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt',
+    ['@nuxtjs/google-tag-manager', { id: 'GTM-KBG9CHV' }]
   ],
   /*
   ** Build configuration
