2e7f49ff96d9c0ebf66c805edb195d52d3078352
rokuzeudon <rokuzeudon@gmail.com>
2019-07-09 00:54:10 +0900

    Add OGP setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,20 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
-    htmlAttrs: { lang: 'ja' },
+    htmlAttrs: {
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:site_name', property: 'og:site_name', content: '6Q.design' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://6qd.github.io/bootstrap-xd-guide/' },
+      { hid: 'og:title', property: 'og:title', content: 'Bootstrap-XD' },
+      { hid: 'og:description', property: 'og:description', content: 'Perfect Bootstrap Design Template — Assets Library — for Adobe XD.' },
+      { hid: 'og:image', property: 'og:image', content: '/bootstrap-xd-guide/ogp.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/bootstrap-xd-guide/favicon.ico' }
